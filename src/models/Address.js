68198b878cf4958353136d26d22c0ca52b628ae8
Caminha <gabrielcaminha255@gmail.com>
2022-12-24 00:52:35 -0300

    feat: added Address model

---
+++ src/models/Address.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const connection = require('../database');
+const User = require('./User');
+
+class Address extends Model {}
+
+Address.init({
+  zipcode: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  street: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  number: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  }
+}, {
+  sequelize: connection,
+  modelName: 'Address',
+  underscored: true,
+});
+
+Address.belongsTo(User, {
+  foreignKey: 'user_id',
+  as: 'user',
+});
+
+module.exports = Address;
