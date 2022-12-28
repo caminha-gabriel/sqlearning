7a466b08b9e1a28a5e2c4e7456c0b5efb35ca8f6
Caminha <gabrielcaminha255@gmail.com>
2022-12-28 01:58:11 -0300

    feat: implemented GET /users/:yserId/addresses function on addresses routes

--- src/routes/addresses.routes.js
+++ src/routes/addresses.routes.js
@@ -3,6 +3,7 @@ const AddressController = require('../controllers/AddressController');
 
 const addressesRoutes = Router();
 addressesRoutes.get('/addresses', AddressController.getAll)
+addressesRoutes.get('/users/:userId/addresses', AddressController.getAllFromUser)
 addressesRoutes.post('/users/:userId/addresses', AddressController.store)
 
 module.exports = addressesRoutes;
