3b4217eae4896f38e3a8d628cecef18182c0e391
Caminha <gabrielcaminha255@gmail.com>
2022-12-31 03:04:32 -0300

    feat: added tech routes to routes index file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const welcomeRoutes = require('./hello.routes');
 const usersRoutes = require('./users.routes');
 const addressesRoutes = require('./addresses.routes');
+const techsRoutes = require('./techs.routes');
 
 module.exports = {
   welcomeRoutes,
   usersRoutes,
   addressesRoutes,
+  techsRoutes,
 }
