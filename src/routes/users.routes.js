db2cb1a88178e628941ff7f43c2ada89d288bc93
Caminha <gabrielcaminha255@gmail.com>
2023-02-06 02:43:04 -0300

    feat: added getUser and delete routes to users.routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,7 +2,9 @@ const { Router } = require('express');
 const UserController = require('../controllers/UserController');
 
 const usersRoutes = Router();
-usersRoutes.get('/users', UserController.getAll)
-usersRoutes.post('/users', UserController.store)
+usersRoutes.get('/users', UserController.getAll);
+usersRoutes.get('/users/:userId', UserController.getUser);
+usersRoutes.post('/users', UserController.store);
+usersRoutes.delete('/users/:userId', UserController.delete);
 
 module.exports = usersRoutes;
