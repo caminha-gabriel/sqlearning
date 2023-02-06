bbb65f08f8d6304cf324e3eafe0e2e743b1a01a2
Caminha <gabrielcaminha255@gmail.com>
2023-02-06 03:01:22 -0300

    feat: added delete route to addresses.routes

--- src/routes/addresses.routes.js
+++ src/routes/addresses.routes.js
@@ -2,8 +2,9 @@ const { Router } = require('express');
 const AddressController = require('../controllers/AddressController');
 
 const addressesRoutes = Router();
-addressesRoutes.get('/addresses', AddressController.getAll)
-addressesRoutes.get('/users/:userId/addresses', AddressController.getAllFromUser)
-addressesRoutes.post('/users/:userId/addresses', AddressController.store)
+addressesRoutes.get('/addresses', AddressController.getAll);
+addressesRoutes.get('/users/:userId/addresses', AddressController.getAllFromUser);
+addressesRoutes.post('/users/:userId/addresses', AddressController.store);
+addressesRoutes.delete('/users/:userId/addresses', AddressController.delete);
 
 module.exports = addressesRoutes;
