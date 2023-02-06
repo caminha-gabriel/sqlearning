230f2c5a48ab88352cddc30f1d553bc297d00577
Caminha <gabrielcaminha255@gmail.com>
2023-02-06 02:42:29 -0300

    feat: added delete function to UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -15,7 +15,7 @@ module.exports = {
 
     if (!user) return res.status(StatusCodes.BAD_REQUEST).json({ message: 'User not found' });
 
-    return res.status(StatusCodes.OK).json(user)
+    return res.status(StatusCodes.OK).json(user);
   },
 
   async store (req, res) {
@@ -24,5 +24,17 @@ module.exports = {
     const user = await User.create({ name, email });
   
     return res.json(user);
-  }
+  },
+
+  async delete (req, res) {
+    const { userId } = req.params;
+
+    const user = await User.findByPk(userId);
+
+    if (!user) return res.status(StatusCodes.BAD_REQUEST).json({ message: 'User not found' });
+
+    await user.destroy();
+
+    return res.status(StatusCodes.NO_CONTENT).send();
+  },
 }
